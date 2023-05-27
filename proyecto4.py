46fb3800152f07716d384ee47423b59bb7872860
Charlie Flo_Artz <charlessflo@outlook.com>
2023-05-27 05:31:15 -0600

    Create proyecto4.py

---
+++ proyecto4.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def generar_contrasena(longitud):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    contrasena = ''.join(random.choice(caracteres) for _ in range(longitud))
+    return contrasena
+
+longitud_deseada = int(input("Ingrese la longitud deseada para la contraseña: "))
+contrasena_generada = generar_contrasena(longitud_deseada)
+print("Contraseña generada:", contrasena_generada)
